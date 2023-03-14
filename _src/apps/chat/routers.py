111425c5a0ee5a3e87fd0feca187f1b5986991f5
alanominator <alanominator@protonmail.com>
2023-03-14 16:30:13 +0500

    q

--- _src/apps/chat/routers.py
+++ _src/apps/chat/routers.py
@@ -23,38 +23,6 @@ chat_router = fastapi.APIRouter(
 )
 
 
-"""
-# todo
-
-accept connection if authenticated
-    else - deny
-
-mongodb
-
-send data to rooms (broadcast to user of room that are online)
-
-"""
-
-# from ..auth.routers import get_current_user
-
-
-
-
-
-
-opened_rooms = {
-    # "room_link": ["websocket1", "websocket2"]
-}
-
-# users_info_by_websocket_id
-user_info_by_websocket = {
-    # "id_of_websocket": {
-    #     "user": "user object"
-    #     "db_user": "from database"
-    #     "access_token_iat": 1231132,
-    #     "rooms_links": []
-    # }
-}
 
 
 
@@ -62,52 +30,112 @@ user_info_by_websocket = {
 class ConnectionManager:
     def __init__(self):
         self.active_connections: List[WebSocket] = []
+        self.websockets_group_by_room = {}
+        self.user_info_by_websocket = {}
 
 
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, websocket: WebSocket, access_token):
         await websocket.accept()
+
         self.active_connections.append(websocket)
-    
+
+        # get user by access token
+        user = get_user_by_access_token(access_token)
+
+        # get access token issued at field
+        access_token_iat = jwt.decode(
+            token = access_token,
+            key = SECRET_KEY
+        )["iat"]
+
+        # open db session and add user to session
+        db = SessionLocal()
+        db.add(
+            user
+        )
+
+        # "user rooms" and "user rooms links" variables
+        user_rooms = user.rooms
+        user_rooms_links = [room.link for room in user_rooms]
+
+        # close db session here
+        db.close()
+
+        # add websocket to websockets_group_by_rooms
+        for room_link in user_rooms_links:
+            if not room_link in self.websockets_group_by_room:
+                self.websockets_group_by_room[room_link] = []
+            self.websockets_group_by_room[room_link].append(websocket)
+
+        # add user info to "user_info_by_websocket"
+        self.user_info_by_websocket[id(websocket)] = {
+            "db_user": user,
+            "access_token_iat": access_token_iat,
+            "rooms_links": user_rooms_links
+        }
+
+        # send "user rooms list" to user
+        # TODO optimize - too much quieres to database
+        user_rooms_list = [schemas.RoomResponse.parse_obj(room.__dict__).dict() for room in user_rooms]
+
+        await manager.send_personal_message(
+            websocket,
+            {
+                "action": "update_rooms_list",
+                "data": user_rooms_list
+            }
+        )
+
 
     def disconnect(self, websocket: WebSocket):
-        # TODO remove key from user_info_by_websocket
 
-        user_rooms_links_list = user_info_by_websocket[id(websocket)]["rooms_links"]
+        try:
+            user_rooms_links_list = self.user_info_by_websocket[id(websocket)]["rooms_links"]
 
-        for room_link in user_rooms_links_list:
-            opened_rooms[room_link].remove(websocket)
+            for room_link in user_rooms_links_list:
+                self.websockets_group_by_room[room_link].remove(websocket)
 
-        del user_info_by_websocket[id(websocket)]
+            del self.user_info_by_websocket[id(websocket)]
 
-        self.active_connections.remove(websocket)
-    
+            self.active_connections.remove(websocket)
+        except Exception as e:
+            print(e)
+
+        
+
 
     async def send_personal_message(self, websocket: WebSocket, message: str):
-        if is_token_time_expired(websocket):
+        if self.is_token_time_expired_by_websocket(websocket):
             websocket.close()
 
-        await websocket.send_text(message)
-    
-
-    # async def broadcast(self, message: str):
-
-    #     for connection in self.active_connections:
-            
-    #         if is_token_time_expired(connection):
-    #             connection.close()
-
-    #         await connection.send_text(message)
+        await websocket.send_json(message)
 
 
     async def broadcast_list_of_ws(self, list_of_websockets: List[WebSocket], message):
-        print("\n\n\nlist_of_websockets - ", list_of_websockets)
 
         for ws in list_of_websockets:
-            if is_token_time_expired(ws):
+            if self.is_token_time_expired_by_websocket(ws):
                 print("\n\n\n closing connection")
-                # await ws.close()
+                await ws.close()
             else:
                 await ws.send_json(message)
+
+
+    def is_token_time_expired_by_websocket(self, websocket: WebSocket) -> bool:
+        """
+        
+        returns "True" if the token time is expired, else - False
+
+        """
+
+        try:
+            return get_time_has_passed_by_epoch_in_min(
+                self.user_info_by_websocket[id(websocket)]["access_token_iat"]
+            ) > ACCESS_TOKEN_EXPIRE_MINUTES
+        except:
+            return False
+
+
 
 
 
@@ -115,66 +143,29 @@ manager = ConnectionManager()
 
 
 
-
-def add_or_update_user_info(websocket_id, access_token):
-
-    user = get_user_by_access_token(access_token)
-
-    decoded_payload = jwt.decode(
-        token = access_token,
-        key = SECRET_KEY
-    )
-
-
-    access_token_iat = decoded_payload["iat"]
-
-    
-    user_info_by_websocket[websocket_id] = {
-        # "user": schemas.UserInfoByWS.parse_obj(user.__dict__).dict(),
-        "db_user": user,
-        "access_token_iat": access_token_iat
-    }
-
-
-
-
-
-
-
-
-def is_token_time_expired(websocket: WebSocket):
-    """
-    
-    returns "True" if the token time is expired, else - False
-
-    """
-
-    try:
-        return get_time_has_passed_by_epoch_in_min(
-            user_info_by_websocket[id(websocket)]["access_token_iat"]
-        ) > ACCESS_TOKEN_EXPIRE_MINUTES
-    except:
-        return False
-
-
-
 # ACTIONS ->>
 # ______________________________
 
-async def user_is_typing(websocket: WebSocket, data):
+async def update_access_token(websocket: WebSocket, data):
+    
+    access_token = data["access_token"]
 
-    # todo check if user in the room
-    # todo broadcast to users of the room "user is typing"
+    print(access_token)
+
+
+
+async def user_is_typing(websocket: WebSocket, data):
 
     room_link = data["room_link"]
 
-    if websocket in opened_rooms[room_link]:
-        print("user is typing in ", data["room_link"])
+    if websocket in manager.websockets_group_by_room[room_link]:
 
-        username = user_info_by_websocket[id(websocket)]["db_user"].email
+        username = manager.user_info_by_websocket[id(websocket)]["db_user"].email # todo username
+
+        print(username + " is typing in ", data["room_link"])
 
         await manager.broadcast_list_of_ws(
-            opened_rooms[room_link],
+            manager.websockets_group_by_room[room_link],
             {
                 "action": "user_is_typing",
                 "data": {
@@ -184,19 +175,6 @@ async def user_is_typing(websocket: WebSocket, data):
             }
         )
 
-
-
-async def update_access_token(websocket: WebSocket, data):
-    
-    access_token = data["access_token"]
-
-    try:
-        add_or_update_user_info(
-            id(websocket),
-            access_token
-        )
-    except:
-        websocket.close()
 
 
 
@@ -213,60 +191,24 @@ async def websocket_endpoint(websocket: WebSocket):
 
     access_token = websocket.cookies.get("access_token")
 
-    print(websocket.cookies)
-
     if not access_token:
         await websocket.close()
         return
 
 
     try:
-        # 
-        add_or_update_user_info(
-            id(websocket),
-            access_token
-        )
-        
-
-        db = SessionLocal()
-
-        user = user_info_by_websocket[id(websocket)]["db_user"]
-
-        db.add(
-            user
-        )
-
-        user_rooms = user.rooms
-
-        for room_link in [r.link for r in user_rooms]:
-            if not room_link in opened_rooms:
-                opened_rooms[room_link] = []
-            opened_rooms[room_link].append(websocket)
-
-        rooms_list = [schemas.RoomResponse.parse_obj(room.__dict__).dict() for room in user_rooms]
-
-        user_info_by_websocket[id(websocket)]["rooms_links"] = [room.link for room in user_rooms]
-
-        await manager.connect(websocket)
-        
-        await websocket.send_json({
-            "action": "update_rooms_list",
-            "data": rooms_list
-        })
-
-        print([schemas.RoomResponse.parse_obj(room.__dict__).dict() for room in user_rooms])
-
+        await manager.connect(websocket, access_token)
     except Exception as e:
         print(e)
+        manager.disconnect(websocket)
         return
-    
 
 
     try:
         while True:
             data = await websocket.receive_json()
 
-            if is_token_time_expired(websocket):
+            if manager.is_token_time_expired_by_websocket(websocket):
                 manager.disconnect(websocket)
 
             action = data["action"]
